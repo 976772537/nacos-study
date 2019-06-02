ea04317e8ceaa082f969af23443d3568da5575f3
976772537 <976772537@qq.com>
2019-06-02 14:25:46 +0800

    dependency

--- nacos/serve/src/main/java/com/drp/serve/ServeApplication.java
+++ nacos/serve/src/main/java/com/drp/serve/ServeApplication.java
@@ -2,8 +2,10 @@ package com.drp.serve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServeApplication {
 
     public static void main(String[] args) {
